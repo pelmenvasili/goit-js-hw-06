be2ae763cc0175c4260e9574ab1b6a4d7cc757c3
Myroslava Lukash <miralukash@gmail.com>
2023-02-05 23:36:22 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,6 @@ const categoriesRef = document.querySelectorAll(".item");
 console.log("Number of categories:", categoriesRef.length);
 const nameEl = categoriesRef.forEach((el) =>
   console.log(
-    `Category: ${el.firstElementChild.textContent}'),  Elements: ${el.lastElementChild.children.length}`
+    `Category: ${el.firstElementChild.textContent}, Elements: ${el.lastElementChild.children.length}`
   )
 );
